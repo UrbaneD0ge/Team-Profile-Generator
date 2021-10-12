d4ec89e26899d5669cb022872f047c85c49f4149
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-10-12 18:02:17 -0400

    prepare and style html templiteral

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const inquirer = require('inquirer');
 const jest = require('jest');
+const fs = require('fs');
+const util = require('util');
 
-inquirer
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const inquirify = () => {
+    return inquirer
     .prompt[
         {
         type: 'input',
@@ -30,3 +35,38 @@ inquirer
         choices: ['Add an intern..', 'Add an engineer..', 'I\'m finished!']
         }
     ]
+
+    function generateHTML(answers) {
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+            <link rel="stylesheet" href="style.css">
+            <title>Team Profile</title>
+        </head>
+        <header class="h1 bg-dark text-light text-center">
+            Team Profile GO:
+        </header>
+        <body class="bg-lt">
+            <div class="container">
+            <div class="row row-cols-3">
+                <div class="col my-2">
+                    <div class="card card-body bg-dark">
+                      <h5 class="card-title text-center">Manager</h5>
+                      <ul class="list-group list-group">
+                        <li class="list-group-item">${answers.name}</li>
+                        <li class="list-group-item">${answers.email}</li>
+                        <li class="list-group-item">${answers.offc}</li>
+                    </div>        
+                </div>
+        
+            </div>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.min.js" integrity="sha384-PsUw7Xwds7x08Ew3exXhqzbhuEYmA2xnwc8BuD6SEr+UmEHlX8/MCltYEodzWA4u" crossorigin="anonymous"></script>
+        </body>
+        </html>
+        `
+    }
